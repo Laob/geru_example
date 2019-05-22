2a735c222d5986dcc0c9ede267a8042eb28171fe
Laob <lukas.kumai@googlemail.com>
2019-05-22 18:21:14 +0200

    Kommentar ergaenzt

--- script.js
+++ script.js
@@ -3,7 +3,10 @@ var charisma=12;
 var intelligence=15;
 var agility=11;
 
-document.addEventListener('DOMContentLoaded', function(){ 
+/* Dieser Event-Listener ist nur dafuer da, dass das Skript
+   wartet, bis das HTML-Dokument fertig geladen ist.
+   Die wichtigen Zeilen sind innerhalb der geschweiften Klammern */
+document.addEventListener('DOMContentLoaded', function() { 
     document.getElementById("strength").innerHTML = strength;
     document.getElementById("charisma").innerHTML = charisma;
     document.getElementById("intelligence").innerHTML = intelligence;
