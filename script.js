0329913aec16956eddf3be4710b55dc4423e8688
Laob <lukas.kumai@googlemail.com>
2019-05-22 18:19:17 +0200

    Beispielimplementierung

---
+++ script.js
@@ -0,0 +1,11 @@
+var strength=17;
+var charisma=12;
+var intelligence=15;
+var agility=11;
+
+document.addEventListener('DOMContentLoaded', function(){ 
+    document.getElementById("strength").innerHTML = strength;
+    document.getElementById("charisma").innerHTML = charisma;
+    document.getElementById("intelligence").innerHTML = intelligence;
+    document.getElementById("agility").innerHTML = agility;    
+}, false);
